41cf1202761e9d11a8eeb5588a8ef9c54e7920d4
Zaki <menza.zm@gmail.com>
2023-04-27 10:53:02 +0600

    Initial Commit

---
+++ PetHouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetHouse
+//
+//  Created by Zaki on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
